4319667b6a52dae5749f013b8db2536984898a5e
taroa36912 <ken.kulome417@gmail.com>
2023-09-22 23:57:39 +0900

    added the sample code of php.

---
+++ webhome-3/sample-post2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>POST_SAMPLE</title>
+</head>
+<body>
+<?php
+	echo "ワンツーマンレッスンと言えば「".$_POST["data"] ."」です。";
+?>
+</form>
+</body>
+</html>

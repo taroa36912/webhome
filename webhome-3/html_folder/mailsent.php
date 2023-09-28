d3ba8dedcee52a939b625e043770fc1fcde90568
taroa36912 <ken.kulome417@gmail.com>
2023-09-28 18:25:05 +0900

    changed the Path.

--- webhome-3/html_folder/mailsent.php
+++ webhome-3/html_folder/mailsent.php
@@ -3,13 +3,13 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <link rel="stylesheet" href="css_folder/inquiry.css"><!--css名変える-->
-        <title>title</title><!--1.タイトル-->
-        <meta name="description" content="sample"><!--2.説明文-->
-        <meta name="keywords" content="sample"><!--3.キーワード-->
-        <link rel="shortcut icon" href="img\Pac.ico" type="image/x-icon">
-        <link rel="apple-touch-icon" href="img\Pac.png" sizes="180x180">
-        <link rel="icon" type="image/png" href="img\Pac.png" sizes="192x192">
+        <link rel="stylesheet" href="../css_folder/inquiry.css"><!--css名変える-->
+        <title>送信完了</title><!--1.タイトル-->
+        <meta name="description" content="送信が完了しました"><!--2.説明文-->
+        <meta name="keywords" content="送信完了"><!--3.キーワード-->
+        <link rel="shortcut icon" href="../img\/Pac.ico" type="image/x-icon">
+        <link rel="apple-touch-icon" href="../img/Pac.png" sizes="180x180">
+        <link rel="icon" type="image/png" href="../img/Pac.png" sizes="192x192">
     </head>
     <body>
     <div class="wrap">
@@ -22,7 +22,7 @@
             </div>
             <div class="arti">
                 <h1>お問い合わせフォーム</h1>
-                <a href=index.html><img class="logo" src="img\Pac.png"></a><p class="open_day">公開日 2023/**/△△</p>
+                <a href=index.html><img class="logo" src="../img/Pac.png"></a><p class="open_day">公開日 2023/09/26</p>
                 <div class="php">
                 <?php
                     mb_language("Japanese");

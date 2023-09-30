c8f0877de0425a7af573d937678c8dfdf75bbf60
taroa36912 <ken.kulome417@gmail.com>
2023-09-30 19:48:58 +0900

    added search func.

---
+++ webhome-3/js_folder/search.js
@@ -0,0 +1,41 @@
+const sites = [
+    { url: '../pacture/cleaner.html', content: '掃除機, 洗濯機'},
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    { url: 'site2.html', content: 'サイト2の内容' },
+    // ... 他のサイトのデータ
+];
+
+window.onload = function() {
+    // URLのクエリパラメータから検索キーワードを取得
+    const params = new URLSearchParams(window.location.search);
+    const keyword = params.get('q');
+
+    const resultsDiv = document.getElementById('results');
+
+    for (let site of sites) {
+        if (site.content.includes(keyword)) {
+            const iframe = document.createElement('iframe');
+            iframe.src = site.url;
+            iframe.style.width = '500px';
+            resultsDiv.appendChild(iframe);
+        }
+    }
+}

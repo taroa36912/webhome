32c352a2a87e7afbc0547c7229d948c9a14da107
taroa36912 <ken.kulome417@gmail.com>
2023-10-05 05:45:58 +0900

    changed and added the lists.

--- webhome-3/js_folder/list.js
+++ webhome-3/js_folder/list.js
@@ -1,27 +1,14 @@
 const sites = [
     { url: '../list/l-cleaner.html', content: '掃除機, 洗濯機'},
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    { url: 'site2.html', content: 'サイト2の内容' },
-    // ... 他のサイトのデータ
+    { url: '../list/l-DRR.html', content: '防災' },
+    { url: '../list/l-fan.html', content: '扇風機' },
+    { url: '../list/l-moive1.html', content: '映画' },
+    { url: '../list/l-moive2.html', content: '映画' },
+    { url: '../list/l-moive3.html', content: '映画' },
+    { url: '../list/l-moive4.html', content: '映画' },
+    { url: '../list/l-OVEN.html', content: 'オーブン' },
+    { url: '../list/l-washing.html', content: '洗濯機' },
+    { url: '../list/l-whisky.html', content: 'ウイスキー' },
 ];
-
 
 export {sites};

93970a4c93e261668f273568bef9ca5550f7de7a
taroa36912 <ken.kulome417@gmail.com>
2023-11-03 23:26:56 +0900

    relate fixed.

--- webhome-3/js_folder/search.js
+++ webhome-3/js_folder/search.js
@@ -21,7 +21,7 @@ window.onload = function() {
             const iframe = document.createElement('iframe');
             iframe.src = site.url;
             // ここでiframeの基本的な横幅を設定
-            iframe.style.width = '120%';
+            iframe.style.width = '100%';
             iframe.style.maxWidth = '500px';
             iframe.style.height = '120px'; // 高さを200pxに設定
             resultsDiv.appendChild(iframe);
